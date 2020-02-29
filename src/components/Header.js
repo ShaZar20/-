b5da4b76ff5b81614a136b275ffe0993ae3fbbc9
ShaZar20 <shayezer14@gmail.com>
2020-02-29 23:13:56 +0200

    shay

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import logo from '../images/Zofim-logo.png';
 
 const Header = () => (
     <div className="header">
-        <h1>תהליך הערכת עובדים</h1>
         <img src={logo} alt="Logo" className="logo"/>
+        <h1>תהליך הערכת עובדים</h1>
     </div>
 );
 
